8f7c56049f3aeab43da3712ba4a7c5b7c6dfc46b
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-16 23:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function driversWithRevenueOver(drivers, revenue){
 function driverNamesWithRevenueOver (drivers, revenue){
   return drivers.filter(driver => driver.revenue > revenue).map(driver => driver.name);
 }
+function exactMatch(drivers, obj){
+  return drivers.filter(driver => driver[object.key[0]]=== objobject.key[0]])
+}
