f6a2aa748f7cb458ec5c1d1093312a4796895cc9
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-16 23:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here:
 function driversWithRevenueOver(driver, revenue){
-  return drivers.filter(driver => object.assign({name = drivers.name },{revenue= drivers.revenue}
+  return drivers.filter(driver => object.assign({name = drivers.name },{revenue= drivers.revenue})
 }
