e1141b5b577d4ae09754b3b544bf61cd02d31420
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-16 23:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function driversWithRevenueOver(drivers, revenue){
   return drivers.filter(driver => driver.revenue > revenue);
 }
 function riverNamesWithRevenueOver(drivers, revenue){
-  return driversWithRevenueOver().map(driver => driver.name);
+  return driversWithRevenueOver(drivers, revenue).map(driver => driver.name);
 }
